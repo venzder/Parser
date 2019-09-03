2f4dca121ef4d3db1116c6712d8d9c2805daa290
Sergey <sergivanov@list.ru>
2019-09-04 01:36:23 +0300

    Доработал функцию парсинга ссылок. Осталось подставить динамически прокси.

--- Parser.py
+++ Parser.py
@@ -1,42 +1,68 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from fake_useragent import UserAgent
+
 
 
 headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
                      'application/signed-exchange;v=b3',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                         ' Chrome/76.0.3809.132 Safari/537.36'}
+           'user-agent': UserAgent().random}
 
 
-def get_hrefs(url, headers):
-    session = requests.Session()
-    request = session.get(url, headers=headers)
-    if request.status_code == 200:
-        soup = bs(request.content, 'lxml')
-        divs = soup.find_all('li', attrs={'class': 'OffersSerpItem OffersSerpItem_view_desktop OffersSerpItem_format_full OffersSerp__list-item OffersSerp__list-item_type_offer'})
-        hrefs = []
-        for div in divs:
-            href = div.find('a', attrs={'class': 'Link Link_js_inited Link_size_m Link_theme_islands SerpItemLink OffersSerpItem__link'})['href']
-            hrefs.append({
-                'href': href
-            })
-        return hrefs
-    else:
-        return []
-
-
-def get_all_hrefs(url, headers):
-    list_dict = []
+def get_url_pages(start_url, headers):
+    lst = []
     while True:
-        list_dict += get_hrefs(url, headers)
         try:
             session = requests.Session()
             request = session.get(url, headers=headers)
             soup = bs(request.content, 'lxml')
             url = f"https://realty.yandex.ru{soup.find('a', attrs={'class': 'Pager__radio-link'})['href']}"
+            lst.append(url)
         except:
             break
-    return list_dict
+    return lst
+
+
+
+def get_hrefs(start_url, headers):
+    all_hrefs = []
+    url = start_url
+    i = 0
+    while True:
+        session = requests.Session()
+        session.proxies = proxies
+        request = session.get(url, headers=headers)
+        status_code = request.status_code
+        if status_code == 200:
+            soup = bs(request.content, 'lxml')
+            divs = soup.find_all('li', attrs={'class': 'OffersSerpItem OffersSerpItem_view_desktop OffersSerpItem_format_full OffersSerp__list-item OffersSerp__list-item_type_offer'})
+            hrefs = []
+            for div in divs:
+                href = div.find('a', attrs={'class': 'Link Link_js_inited Link_size_m Link_theme_islands SerpItemLink OffersSerpItem__link'})['href']
+                hrefs.append({
+                    'href': href
+                })
+            all_hrefs += hrefs
+            if divs:
+                url = f'{start_url}?page={i+1}'
+            else:
+                break
+        else:
+            print(f'Возникла ошибка {status_code}')
+        i += 1
+
+
+proxies = {'http': '144.217.163.138:8080'}
+
+def get_all_hrefs(url, headers, proxies):
+    session = requests.Session()
+    session.proxies = proxies
+    request = session.get(url, headers=headers)
+    s = request.status_code
+    soup = bs(request.content, 'lxml')
+    divs = soup.find_all('li', attrs={'class': 'OffersSerpItem OffersSerpItem_view_desktop OffersSerpItem_format_full OffersSerp__list-item OffersSerp__list-item_type_offer'})
+
+    print(s, divs)
 
 # def get_cityes_yandex():
 
@@ -55,4 +81,4 @@ def get_all_hrefs(url, headers):
 
 
 # print(get_all_hrefs('https://realty.yandex.ru/sankt-peterburg/kupit/kvartira/?page=0', headers))
-print(get_hrefs('https://realty.yandex.ru/sankt-peterburg/kupit/kvartira/?page=0', headers))
+print(get_all_hrefs('https://realty.yandex.ru/sankt-peterburg/kupit/kvartira/', headers, proxies))
